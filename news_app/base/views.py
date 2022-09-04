bf3d5ce365f143eeb49e2283ef8f5e4e5d1f32a7
yaohuang13245 <97291050+yaohuang13245@users.noreply.github.com>
2022-09-04 22:18:56 +0800

    login, register

--- news_app/base/views.py
+++ news_app/base/views.py
@@ -1,5 +1,45 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+# from django.views.generic.list import ListView
+# from .models import News
+from django.shortcuts import  render, redirect
+from .templates.base.forms import NewUserForm
+from django.contrib.auth import login, authenticate, logout
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
+    
+def homepage(request):
+	return render(request=request, template_name='base/home.html')
+    
+def register_request(request):
+	if request.method == "POST":
+		form = NewUserForm(request.POST)
+		if form.is_valid():
+			user = form.save()
+			login(request, user)
+			messages.success(request, "Registration successful." )
+			return redirect("news_app:register")
+		messages.error(request, "Unsuccessful registration. Invalid information.")
+	form = NewUserForm()
+	return render (request=request, template_name="base/register.html", context={"register_form":form})
 
-def newsApp(request):
-    return HttpResponse('unbiased news')
+def login_request(request):
+	if request.method == "POST":
+		form = AuthenticationForm(request, data=request.POST)
+		if form.is_valid():
+			username = form.cleaned_data.get('username')
+			password = form.cleaned_data.get('password')
+			user = authenticate(username=username, password=password)
+			if user is not None:
+				login(request, user)
+				messages.info(request, f"You are now logged in as {username}.")
+				return redirect("news_app:homepage")
+			else:
+				messages.error(request,"Invalid username or password.")
+		else:
+			messages.error(request,"Invalid username or password.")
+	form = AuthenticationForm()
+	return render(request=request, template_name="base/news_list.html", context={"login_form":form})
+
+def logout_request(request):
+	logout(request)
+	messages.info(request, "You have successfully logged out.") 
+	return redirect("news_app:homepage")
